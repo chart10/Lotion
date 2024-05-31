660d2b64f0cc8e0fca6e5865a10c08afda2e6f8d
Christian Hart <christiantaylorhart@gmail.com>
2024-05-31 10:38:00 -0400

    Display currentNote values in NoteEditor comp

--- src/components/NoteEditor/NoteEditor.jsx
+++ src/components/NoteEditor/NoteEditor.jsx
@@ -1,34 +1,10 @@
 import './NoteEditor.scss';
 
-const NoteEditor = () => {
+const NoteEditor = ({ services, currentNote, setCurrentNote }) => {
   return (
     <div className='note-editor'>
-      <h1 className='note-title'>Note Title</h1>
-      <p className='note-text'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam imperdiet
-        est ac lectus dignissim tristique. Integer vel metus non libero
-        vulputate vestibulum. Suspendisse in leo id est molestie mattis non nec
-        libero. Praesent tincidunt risus sed neque dapibus pulvinar. In
-        efficitur quam at purus iaculis tempus. Integer nisl arcu, auctor nec
-        metus non, porttitor faucibus turpis. Morbi eget ex consectetur,
-        pulvinar nunc id, aliquam ex. Curabitur convallis porttitor ipsum, non
-        ullamcorper erat volutpat auctor. Nulla risus augue, feugiat sit amet
-        quam a, ornare dictum dolor. Cras accumsan mauris ornare leo scelerisque
-        volutpat. Phasellus luctus sit amet dui id vestibulum. Aliquam
-        condimentum eros in ante aliquam laoreet. Suspendisse potenti. Nam augue
-        dolor, sagittis vel ligula et, lacinia lacinia odio. Aliquam lobortis
-        tortor a porta vehicula. Vivamus quis efficitur eros. Integer ut
-        vehicula lorem, non tempus dui. Class aptent taciti sociosqu ad litora
-        torquent per conubia nostra, per inceptos himenaeos. Fusce volutpat,
-        felis in sagittis maximus, tortor elit facilisis arcu, vel molestie elit
-        libero nec quam. Sed porttitor libero sit amet laoreet blandit. Cras
-        vitae dapibus erat. Ut interdum, neque nec consequat aliquet, quam nulla
-        posuere dui, eget elementum nisl nisl ac libero. Aenean gravida velit eu
-        lacus sagittis, eu dignissim tellus tempor. Suspendisse varius purus eu
-        lectus mattis, in ornare lacus finibus. Quisque placerat, mauris a
-        molestie volutpat, odio erat egestas erat, quis ultrices risus lectus
-        quis nibh. Donec efficitur in erat ac rutrum.
-      </p>
+      <h1 className='note-title'>{currentNote.title}</h1>
+      <p className='note-text'>{currentNote.text}</p>
     </div>
   );
 };
