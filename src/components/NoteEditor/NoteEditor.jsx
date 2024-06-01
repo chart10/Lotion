dd5545817c5929e176ac63043d554e7385617efe
Christian Hart <christiantaylorhart@gmail.com>
2024-06-01 10:59:27 -0400

    Note title toggles to text input onClick in NoteEditor

--- src/components/NoteEditor/NoteEditor.jsx
+++ src/components/NoteEditor/NoteEditor.jsx
@@ -1,15 +1,35 @@
+import { useState } from 'react';
 import './NoteEditor.scss';
 
 const NoteEditor = ({ services, currentNote }) => {
+  // Test the updateNote service
   const newNote = {
     id: 5,
     title: 'Updated Note',
     text: 'This is an edited note!',
   };
+  const handleEnterKey = (event) => {
+    if (event.key === 'Enter') {
+      setEditingTitle(false);
+    }
+  };
 
+  const [editingTitle, setEditingTitle] = useState(false);
   return (
     <div className='note-editor'>
-      <h1 className='note-title'>{currentNote.title}</h1>
+      {editingTitle ? (
+        <input
+          onKeyDown={handleEnterKey}
+          className='note-title-input'
+          type='text'
+          defaultValue={currentNote.title}
+        />
+      ) : (
+        <h1 className='note-title' onClick={() => setEditingTitle(true)}>
+          {currentNote.title}
+        </h1>
+      )}
+
       <p className='note-text'>{currentNote.text}</p>
       <div className='btn-container'>
         <button
