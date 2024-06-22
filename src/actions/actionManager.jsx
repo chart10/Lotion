71b51b991b5532e44f75e27e6c960b6b06f6fe6f
Christian Hart <christiantaylorhart@gmail.com>
2024-06-22 10:34:07 -0400

    Update index.html Title, include Merriweather font in project files

--- src/actions/actionManager.jsx
+++
@@ -1,9 +0,0 @@
-// import services from '../services/services';
-
-// Reseed default NoteList
-
-// Update note title and update NoteList
-
-// Update note body and update Notelist
-
-// Create new note and place at the top of the NoteList
