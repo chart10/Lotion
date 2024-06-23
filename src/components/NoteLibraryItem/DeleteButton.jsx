0efb360e480097d7b10607007cef0c9675384928
Christian Hart <christiantaylorhart@gmail.com>
2024-06-23 17:10:46 -0400

    Approval-box dismisses when clicking elsewhere on the page

--- src/components/NoteLibraryItem/DeleteButton.jsx
+++ src/components/NoteLibraryItem/DeleteButton.jsx
@@ -1,23 +1,27 @@
-import { useRef } from 'react';
+// import { useRef } from 'react';
 import { useAppContext } from '../../App';
 import { BsX } from 'react-icons/bs';
 
-const useFocus = () => {
-  const htmlElRef = useRef(null);
-  const setFocus = () => {
-    htmlElRef.current && htmlElRef.current.focus();
-  };
-
-  return [htmlElRef, setFocus];
-};
+// const useFocus = () => {
+//   const htmlElRef = useRef(null);
+//   const setFocus = () => {
+//     htmlElRef.current && htmlElRef.current.focus();
+//   };
+//   return [htmlElRef, setFocus];
+// };
 
 const DeleteButton = ({ note, showApproval, setShowApproval }) => {
   const { deleteSelectedNote } = useAppContext();
-  const [approvalBoxRef, setApprovalBoxFocus] = useFocus();
+  // const [approvalBoxRef, setApprovalBoxFocus] = useFocus();
 
   const handleClickDelete = () => {
-    setShowApproval(!showApproval);
-    setApprovalBoxFocus();
+    setShowApproval(true);
+  };
+
+  const handleHideApproval = () => {
+    setTimeout(() => {
+      setShowApproval(false);
+    }, 150);
   };
 
   const handleConfirmDelete = (noteId) => {
@@ -27,32 +31,39 @@ const DeleteButton = ({ note, showApproval, setShowApproval }) => {
   return (
     <div className='delete-btn-container'>
       <button
+        tabIndex={-1}
         className={showApproval ? 'delete-btn btn pending' : 'delete-btn btn'}
         onClick={handleClickDelete}
-      >
-        <BsX size={'1em'} />
-      </button>
-
-      <div
-        className={showApproval ? 'approval-box active' : 'approval-box'}
-        tabIndex='0'
         onBlur={() => {
-          setShowApproval(false);
+          handleHideApproval();
           console.log('approval removed');
         }}
-        ref={approvalBoxRef}
       >
-        Delete?
-        <span className='yes' onClick={() => handleConfirmDelete(note.id)}>
-          {' '}
-          Y{' '}
-        </span>
-        /
-        <span className='no' onClick={() => setShowApproval(false)}>
-          {' '}
-          N
-        </span>
-      </div>
+        <BsX size={'1em'} />
+
+        <div
+          className={showApproval ? 'approval-box active' : 'approval-box'}
+          tabIndex={-1}
+          // ref={approvalBoxRef}
+        >
+          Delete?
+          <span className='yes' onClick={() => handleConfirmDelete(note.id)}>
+            {' '}
+            Y{' '}
+          </span>
+          /
+          <span
+            className='no'
+            onClick={() => {
+              console.log('closing approval box');
+              setShowApproval(false);
+            }}
+          >
+            {' '}
+            N
+          </span>
+        </div>
+      </button>
     </div>
   );
 };
